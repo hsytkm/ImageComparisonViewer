a3d154dd62440ba51b30a7c3444592b4274d5cbe
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-10-15 00:09:13 +0900

    Add Project

---
+++ Source/ICV.Control.ZoomableImage/UserControl1.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ICV.Control.ZoomableImage
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
